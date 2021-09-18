b19eb5ffcaa8f78d9a43f10efa5a3419d9774c31
Дмитрий <bd240897@yandex.ru>
2021-09-18 16:12:12 +0300

    Вынес создание элекмнтов в отдельные функции - сохранение переод выносом handle_events

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from player_file import *
 from constants import *
 from coin_file import *
 from mobs_file import *
+from platform_file import Platform
 import random
 
 # Создаем игру и окно
@@ -14,52 +15,52 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
-class Platform(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-        self.image.fill(pygame.Color(PLATFORM_COLOR))
-        self.rect = pygame.Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
-
 # cоздаим группы и добави туда объекты
 all_bloks = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_coins = pygame.sprite.Group()
 all_mobs = pygame.sprite.Group()
 
-player = Player(all_bloks, all_coins, all_mobs)
+def create_player():
+    global player
+    player = Player(all_bloks, all_coins, all_mobs)
+    all_sprites.add(player)
 
-########### УБРАТЬ
-sword = Sword()
-player.keep_sword(sword)
+def create_mobs():
+    for num_mob in range(1,4):
+        mob = Mob(*Mob.random_mob_position(), all_bloks)
+        all_mobs.add(mob)
+        all_sprites.add(mob)
 
-all_sprites.add(sword)
+def create_sword(player):
+    sword = Sword()
+    player.keep_sword(sword)
+    all_sprites.add(sword)
 
+def create_platforms():
+    # рисование уровня
+    x = y = 0  # координаты
+    for row in level:  # вся строка
+        for col in row:  # каждый символ
+            if col == "-":
+                one_platform = Platform(x, y)
+                all_bloks.add(one_platform)
+                all_sprites.add(one_platform)
+            if col == "*":
+                one_coin = Coins(x, y)
+                all_coins.add(one_coin)
+                all_sprites.add(one_coin)
+            x += PLATFORM_WIDTH  # блоки платформы ставятся на ширине блоков
+        y += PLATFORM_HEIGHT  # то же самое и с высотой
+        x = 0  # на каждой новой строчке начинаем с нуля
 
-all_sprites.add(player)
-for num_mob in range(1,4):
-    mob = Mob(*Mob.random_mob_position(), all_bloks)
-    all_mobs.add(mob)
-    all_sprites.add(mob)
+def create_objects():
+    create_platforms()
+    create_player()
+    create_mobs()
+    create_sword(player)
 
-# рисование уровня
-x = y = 0  # координаты
-for row in level:  # вся строка
-    for col in row:  # каждый символ
-        if col == "-":
-            one_platform = Platform(x, y)
-            all_bloks.add(one_platform)
-            all_sprites.add(one_platform)
-        if col == "*":
-            one_coin = Coins(x, y)
-            all_coins.add(one_coin)
-            all_sprites.add(one_coin)
-        x += PLATFORM_WIDTH  # блоки платформы ставятся на ширине блоков
-    y += PLATFORM_HEIGHT  # то же самое и с высотой
-    x = 0  # на каждой новой строчке начинаем с нуля
-
-
-
+create_objects()
 
 # Цикл игры
 running = True
@@ -67,6 +68,8 @@ while running:
     # Держим цикл на правильной скорости
     clock.tick(FPS)
     # Ввод процесса (события)
+
+
     for event in pygame.event.get():
         # check for closing window
         if event.type == pygame.QUIT:
